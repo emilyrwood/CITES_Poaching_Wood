a6bdef6414ec49b9d83b5d96fbe74d9bcecdc0bd
emilyrwood <erwhome1@gmail.com>
2022-12-14 00:28:17 -0500

    Explored top importer and exporter countries with ggplot and data wrangling

--- Code/data_exploration.R
+++ Code/data_exploration.R
@@ -6,26 +6,57 @@
 #install.packages('rworldmap')
 library(rworldmap)
 
-#Imports
-CITES_Importer_withmap <- joinCountryData2Map( CITESPoaching2016,
-                                               joinCode = "ISO_A2",
-                                               nameJoinColumn = "Importer",)
-data(CITESPoaching2016)
-sPDF <- joinCountryData2Map( CITESPoaching2016
+#Importer Countries
+CITES_Importer_withmap <- joinCountryData2Map( CITES2016_Processed
                              ,joinCode = "ISO_A2"
                              ,nameJoinColumn = "Importer")
 mapDevice() #create world map shaped window
-mapCountryData(sPDF
-               ,nameColumnToPlot='Importer')
+mapCountryData(CITES_Importer_withmap, nameColumnToPlot='Importer')
 
-#Exports 
-CITES_Exporter_withmap <- joinCountryData2Map( CITESPoaching2016,
-                                               joinCode = "ISO_A2",
-                                               nameJoinColumn = "Exporter",)
-data(CITESPoaching2016)
-sPDF <- joinCountryData2Map( CITESPoaching2016
-                             ,joinCode = "ISO_A2"
-                             ,nameJoinColumn = "Exporter")
+Importerplot1 <- ggplot(CITES2016_Processed, aes(x=Importer))+ 
+                geom_bar(binwidth=50, colour="blue", fill="light blue", alpha=0.3)
+Importerplot1
+
+#Difficult to interpret. Wrangle new dataset and create bar graph with only top 10 import countries:
+
+table(CITES2016_Processed$Importer)
+
+ImporterData <-CITES2016_Processed %>%
+                group_by(Importer)  %>%
+                summarise(Importer, count=n()) %>%
+                filter(Importer == "US" | Importer == "JP" | Importer == "DE" | Importer == "FR"
+                       | Importer == "HK" | Importer == "CH" | Importer == "CN" | Importer == "SG" | Importer == "AE" | Importer == "CA")
+
+
+Importerplot2 <- ggplot(ImporterData, aes(x=Importer))+ 
+  geom_bar(binwidth=50, colour="purple", fill="lavender", alpha=0.3)
+Importerplot2
+  
+#Exporter Countries 
+CITES_Exporter_withmap <- joinCountryData2Map( CITES2016_Processed
+                                               ,joinCode = "ISO_A2"
+                                               ,nameJoinColumn = "Exporter")
 mapDevice() #create world map shaped window
-mapCountryData(sPDF
-               ,nameColumnToPlot='Exporter')
+mapCountryData(CITES_Exporter_withmap, nameColumnToPlot='Exporter')
+
+Exporterplot1 <- ggplot(CITES2016_Processed, aes(x=Exporter))+ 
+  geom_bar(binwidth=50, colour="blue", fill="light blue", alpha=0.3)
+  Exporterplot1
+
+#Difficult to interpret. Wrangle new dataset and create bar graph with only top 10 export countries:
+
+table(CITES2016_Processed$Exporter)
+
+ExporterData <-CITES2016_Processed %>%
+  group_by(Exporter)  %>%
+  summarise(Exporter, count=n()) %>%
+  filter(Exporter == "NL" | Exporter == "ID" | Exporter == "IT" | Exporter == "US"
+         | Exporter == "FR" | Exporter == "DE" | Exporter == "EC" | Exporter == "AU" | Exporter == "TH"| Exporter== "ZA")
+
+
+Exporterplot2 <- ggplot(ExporterData, aes(x=Exporter))+ 
+  geom_bar(binwidth=50, colour="blue", fill="light blue", alpha=0.3)
+Exporterplot2
+
+
+
